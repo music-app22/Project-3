d309dd962a5efeeeed90827a746c6c7024d7f354
Tyler Cornwell <tylercornwell@hotmail.com>
2019-08-26 14:48:19 -0700

    working site 2

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,8 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/songs" component={Songs} />
+          <Route exact path="/songs" component={HomePage} />
+          <Route exact path="/" component={Songs} />
           <Route exact path="/songs/:id" component={Detail} />
           <Route component={NoMatch} />
         </Switch>
